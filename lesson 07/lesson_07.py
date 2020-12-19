5bafc6aa0041f66ebe366e3f6d4194ecb6b065b6
Maxim Gasilin <maxim.gasilin@mail.ru>
2020-12-19 19:27:26 +0300

    Первоначальный вариант

--- lesson 07/lesson_07.py
+++ lesson 07/lesson_07.py
@@ -6,18 +6,10 @@ from gb_parse.spiders.instagram import InstagramSpider
 
 if __name__ == '__main__':
     dotenv.load_dotenv('.env')
-<<<<<<< HEAD
-=======
-    hash_tags = ['cats']
->>>>>>> origin/lesson_07
     crawl_settings = Settings()
     crawl_settings.setmodule('gb_parse.settings')
     crawl_proc = CrawlerProcess(settings=crawl_settings)
     crawl_proc.crawl(InstagramSpider,
-<<<<<<< HEAD
-=======
-                     tags=hash_tags,
->>>>>>> origin/lesson_07
                      login=os.getenv('INST_LOGIN'),
                      enc_password=os.getenv('INST_PSWD'))
     crawl_proc.start()
